70030b7f6fc2150700fb43e02127841a25bfe751
verus-fastrun <47342110+verus-fastrun@users.noreply.github.com>
2019-07-08 12:58:36 -0400

    Added more tests and changed spacing
    Added test for all perimeter methods and area methods. Added spacing between getShape(), getPerimeter(), and getArea() tests and added more comments.

--- Shapes/src/test.java
+++ Shapes/src/test.java
@@ -3,24 +3,40 @@ public class test{
 
 	public static void main(String[] args) {
 	
-	//rectangle object
+	//rectangle object is created
 	Rectangle rec = new Rectangle("Rectangle", 4, 2, 4);
-	//square object
+		
+	//square object is created
 	Square sq = new Square("Square", 4, 2, 2);
+		
 	//test for a square object that is not a square
-	Square re = new Square("Square", 4, 2, 3);
-	//circle object
+	Square faker = new Square("Square", 4, 2, 3);
+		
+	//circle object is created
 	Circle cir = new Circle("Circle", 1, 5);
-	//right triangle object
+		
+	//right triangle object is created
 	RightTriangle riTri = new RightTriangle("Right Triangle", 3, 3 , 4);
 	
 	//test for the various getShapes
 	rec.getShape();
 	sq.getShape();
-	re.getShape();
+	faker.getShape();
 	cir.getShape();
 	riTri.getShape();
-	//test the perimeter method for right triangle
-	System.out.println(riTri.getPerimeter());
+	
+	//test the perimeter method for the shapes
+	System.out.printf("%nThe perimeter of the " + rec.name +" is: " + rec.getPerimeter()+"%n");
+	System.out.println("The perimeter of the " + sq.name + " is: " + sq.getPerimeter());
+	System.out.println("The perimeter of the " + faker.name + " is: " + faker.getPerimeter());
+	System.out.println("The perimeter of the " + cir.name + " is: " + cir.getPerimeter());
+	System.out.printf("The perimeter of the " + riTri.name + " is: "+ riTri.getPerimeter() + "%n%n");
+	
+	//test the area method for the shapes
+	rec.getArea();
+	sq.getArea();
+	faker.getArea();
+	cir.getArea();
+	riTri.getArea();
 	}
 }
