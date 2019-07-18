be22819a243a6393502ce61fa20008a8fa5511b9
Mitchell Bisson <mbisson@mail.umw.edu>
2019-07-18 09:58:26 -0400

    added comments to toPig method

--- Java Projects/src/PigLatinTranslator.java
+++ Java Projects/src/PigLatinTranslator.java
@@ -17,21 +17,22 @@ public class PigLatinTranslator extends Application{
 	}
 	
 	public static String toPig(String transWord) {
+		//creating characters for vowels
 		char a = 'a';
 		char e = 'e';
 		char i = 'i';
 		char o = 'o';
 		char u = 'u';
-		
+		//splitting the sentence given into seperate words based on the spaces between words
 		String[] words = transWord.split(" ");
-		
+		//pig String, which will be what the final sentence is added to
 		String pig = "";
-		
-		
+		//this loop iterates through each of the words in the list
 		for(int j = 0; j < words.length; j++) {
 			int first = 0;
 			int firstVowel = 0;
 			int last = words[j].length();
+			//this loop iterates through each letter of the word until a vowel is found
 			for(int x = 0; x < last; x++) {
 				char temp = Character.toLowerCase(words[j].charAt(x));
 				if(temp == a || temp == e || temp == i || temp == o || temp == u) {
@@ -39,6 +40,7 @@ public class PigLatinTranslator extends Application{
 					break;
 				}
 			}
+			//checks to see which ending the method should add depending on what letter it starts with
 			if(first!=firstVowel) {
 				String start = words[j].substring(firstVowel,last);
 				String finish = words[j].substring(first,firstVowel)+"ay";
@@ -48,13 +50,14 @@ public class PigLatinTranslator extends Application{
 				words[j] = words[j]+"yay";
 			}
 		}
+		//adds all words to pig seperated by spaces
 		for(int y = 0; y < words.length; y++) {
 			if(y!=words.length-1)
 				pig = pig+words[y]+" ";
 			else
 				pig = pig+words[y];
 		}
-		
+		//returns the pig sentence
 		return pig;
 	}
 	
